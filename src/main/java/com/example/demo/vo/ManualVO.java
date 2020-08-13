e3ed0bd9137b0b39b232f526a58e486f157ff608
th_kim <th_kim@atectn.com>
2020-08-13 11:02:11 +0900

    신규파일 생성된 내역 추가

---
+++ src/main/java/com/example/demo/vo/ManualVO.java
@@ -0,0 +1,35 @@
+package com.example.demo.vo;
+
+import java.util.Date;
+
+public class ManualVO {
+	private int Mno;
+	private String Mfw;
+	private String Mcomment;
+	private Date Mregdate;
+	public int getMno() {
+		return Mno;
+	}
+	public void setMno(int mno) {
+		Mno = mno;
+	}
+	public String getMfw() {
+		return Mfw;
+	}
+	public void setMfw(String mfw) {
+		Mfw = mfw;
+	}
+	public String getMcomment() {
+		return Mcomment;
+	}
+	public void setMcomment(String mcomment) {
+		Mcomment = mcomment;
+	}
+	public Date getMregdate() {
+		return Mregdate;
+	}
+	public void setMregdate(Date mregdate) {
+		Mregdate = mregdate;
+	}
+	
+}
